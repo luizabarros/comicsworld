2d26dee43f5ea3cc8dcbec71466f75d27afc4552
Luiza Barros <thecoderana@gmail.com>
2023-03-24 10:26:14 -0300

    feat: add pagination

--- src/components/Card/Card.tsx
+++ src/components/Card/Card.tsx
@@ -1,46 +1,69 @@
-import { useState, useEffect } from "react"
+import { useState, useEffect, useRef } from "react"
 import api from "../../services/api"
 
 interface IComic {
-    count: number,
-    limit: number,
-    offset: number,
-    results: {
-        title: string,
-        id: number,
-        prices: {
-            type: string,
-            price: number
-        }[],
-        thumbnail: {
-            extension: string,
-            path: string
-        }
-    }[]
-    total: number
+    title: string,
+    id: number,
+    prices: {
+        type: string,
+        price: number
+    }[],
+    thumbnail: {
+        extension: string,
+        path: string
+    }
 }
 
 const Card = () => {
-    const [comics, setComics] = useState<IComic>()
+    const [comics, setComics] = useState<IComic[] | []>([])
+    const [currentOffset, setCurrentOffset] = useState<number>(0)
+    const loadMoreRef = useRef(null)
 
     useEffect(() => {
-        async function getComics() {
-            const response = await api.get("")
-            setComics(response.data.data)
+        const options = {
+            root: null,
+            rootMargin: "20px",
+            threshold: 1.0
         }
-        getComics()
+
+        const observer = new IntersectionObserver((entities) => {
+            const target = entities[0]
+
+            if (target.isIntersecting) {
+                setCurrentOffset(old => old + 8)
+            }
+        }, options)
+
+        if (loadMoreRef.current) {
+            observer.observe(loadMoreRef.current)
+        }
     }, [])
+
+    useEffect(() => {
+        async function infiniteScrolling() {
+            const response = await api.get("", {
+                params: {
+                    limit: 8,
+                    offset: currentOffset
+                }
+            })
+
+            const newComics = response.data.data.results
+            setComics(oldComics => [...oldComics, ...newComics])
+        }
+        infiniteScrolling()
+    }, [currentOffset])
 
     return (
         <ul>
             {
-                comics?.results.map(({ id, title, thumbnail, prices }) => {
+                comics.map(({ title, thumbnail, prices }, index) => {
                     const srcImg = thumbnail.path + "." + thumbnail.extension
                     const validatePrice = prices[0].price == 0 ? 1.99 : prices[0].price 
                     const format = { minimumFractionDigits: 2 , style: 'currency', currency: 'BRL' }
 
                     return (
-                        <li key={ id }>
+                        <li key={ index }>
                             <img src={ srcImg } alt={ title }/>
                             <h2>{ title }</h2>
                             <p>{ validatePrice.toLocaleString('pt-BR', format) }</p>
@@ -49,6 +72,7 @@ const Card = () => {
                     )
                 })
             }
+            <p ref={loadMoreRef}>Carregando mais HQs...</p>
         </ul>
     )
 }
