54a01407da4e2552ada35930890da542dac8fd24
Luiza Barros <thecoderana@gmail.com>
2023-03-26 08:08:59 -0300

    feat: add header style

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,24 +1,27 @@
 import { AiOutlineShoppingCart, AiOutlineSearch } from "react-icons/ai"
 import { useNavigate } from "react-router-dom"
+import Container from "."
 
 const Header = () => {
     const navigate = useNavigate()
 
     return (
-        <header onClick={() => navigate("/")}>
-            <h1>ComicsWorld</h1>
+        <Container>
+            <header onClick={() => navigate("/")}>
+                <h1>ComicsWorld</h1>
 
-            <div>
-                <AiOutlineSearch/>  
-                <input 
-                type="search" 
-                name="searchbar" 
-                id="searchbar"
-                placeholder="Pesquise um item"/>
-            </div>
+                <div>
+                    <AiOutlineSearch/>  
+                    <input 
+                    type="search" 
+                    name="searchbar" 
+                    id="searchbar"
+                    placeholder="Pesquise um item"/>
+                </div>
 
-            <AiOutlineShoppingCart/>
-        </header>
+                <AiOutlineShoppingCart/>
+            </header>
+        </Container>
     )
 }
 
